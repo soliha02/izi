aa29b9dfbdbf9b46f1efd965e25c7431219eafcf
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-06-26 10:39:31 +0500

    Add files via upload

---
+++ negr.js
@@ -0,0 +1,10 @@
+let ism = prompt("Ismingini kiriting")
+let ID = prompt("Mars spacedagi idyizni yozing")
+let Password = prompt("Mars spacedagi parolizni yozing")
+
+let user = {
+    ism,
+    ID,
+    Password,
+}
+console.log(user);
